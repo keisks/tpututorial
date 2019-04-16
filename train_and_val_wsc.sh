dd992cc1f6828980a9697e7096d8d27ae1ebe18b
chandrab <chandrab@allenai.org>
2019-04-15 23:14:34 -0700

    TPU grid search

--- train_and_val_wsc.sh
+++ train_and_val_wsc.sh
@@ -2,21 +2,37 @@
 set -e
 set -x
 export PYTHONPATH=$(pwd)
-python bert/run_bert.py \
-  --output_dir=gs://chandrab-tpu-tutorial/wsc/ \
-  --input_data=data/wsc/ \
-  --do_lower_case=True \
-  --max_seq_length=64 \
-  --do_train=True \
-  --predict_val=True \
-  --predict_test=False \
-  --train_batch_size=16 \
-  --predict_batch_size=256 \
-  --learning_rate=2e-5 \
-  --num_train_epochs=3 \
-  --warmup_proportion=0.1 \
-  --iterations_per_loop=1000 \
-  --use_tpu=True \
-  --tpu_name=$(hostname)-2 \
-  --bert_large=True \
-  --num_labels 2
+batchsizes=( 3 4 8 )
+for s in "${batchsizes[@]}"
+do
+	learningrates=( 1e-5 2e-5 3e-5 5e-5 )
+	for l in "${learningrates[@]}"
+	do
+		epochs=( 3 4 10 )
+		for e in "${epochs[@]}"
+		do
+			warmups=( 0.2 )
+			for w in "${warmups[@]}"
+			do
+				python bert/run_bert.py \
+				  --output_dir=gs://chandrab-tpu-tutorial/wsc_batch-${s}_lr-${l}_epoch-${e}-warmup-${w}/ \
+				  --input_data=data/wsc/ \
+				  --do_lower_case=True \
+				  --max_seq_length=64 \
+				  --do_train=True \
+				  --predict_val=True \
+				  --predict_test=False \
+				  --train_batch_size=${s} \
+				  --predict_batch_size=256 \
+				  --learning_rate=${l} \
+				  --num_train_epochs=${e} \
+				  --warmup_proportion=${w} \
+				  --iterations_per_loop=1000 \
+				  --use_tpu=True \
+				  --tpu_name=$(hostname)-2 \
+				  --bert_large=True \
+				  --num_labels 2
+			done
+		done
+	done
+done
