3ef89b97feef43cf2a98da65e49418c917bb1352
chandrab <chandrab@allenai.org>
2019-04-16 16:02:17 -0700

    Use 30k version of wsc

--- train_and_val_wsc.sh
+++ train_and_val_wsc.sh
@@ -5,13 +5,13 @@ export PYTHONPATH=$(pwd)
 batchsizes=( 8 16 32 64 )
 for s in "${batchsizes[@]}"
 do
-	learningrates=( 1e-5 2e-5 3e-5 5e-5 )
+	learningrates=( 1e-5 2e-5 5e-5 )
 	for l in "${learningrates[@]}"
 	do
-		epochs=( 3 4 10 )
+		epochs=( 10 4 )
 		for e in "${epochs[@]}"
 		do
-			warmups=( 0.2 )
+			warmups=( 0.1 0.2 )
 			for w in "${warmups[@]}"
 			do
 				python bert/run_bert.py \
@@ -29,7 +29,7 @@ do
 				  --warmup_proportion=${w} \
 				  --iterations_per_loop=1000 \
 				  --use_tpu=True \
-				  --tpu_name=chandrab-tpu-wsc-2 \
+				  --tpu_name=chandrab-tpu-tutorial-2 \
 				  --bert_large=True \
 				  --num_labels 2
 			done
