e2b477242863dc2424d1229f13eda64029698b40
chandrab <chandrab@allenai.org>
2019-04-16 21:56:44 -0700

    Wsc wlpmi split

--- train_and_val_wsc.sh
+++ train_and_val_wsc.sh
@@ -15,8 +15,8 @@ do
 			for w in "${warmups[@]}"
 			do
 				python bert/run_bert.py \
-				  --output_dir=gs://chandrab-tpu-tutorial/wsc-30k/batch-${s}_lr-${l}_epoch-${e}-warmup-${w}/ \
-				  --input_data=data/wsc-30k/ \
+				  --output_dir=gs://chandrab-tpu-tutorial/wsc-wlpmi/batch-${s}_lr-${l}_epoch-${e}-warmup-${w}/ \
+				  --input_data=data/wsc-wlpmi/ \
 				  --do_lower_case=True \
 				  --max_seq_length=64 \
 				  --do_train=True \
