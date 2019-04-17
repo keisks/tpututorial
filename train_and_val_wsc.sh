b468ca3037c697610cb527507756737db435b01e
chandrab <chandrab@allenai.org>
2019-04-16 21:59:23 -0700

    Use the right tpu

--- train_and_val_wsc.sh
+++ train_and_val_wsc.sh
@@ -29,7 +29,7 @@ do
 				  --warmup_proportion=${w} \
 				  --iterations_per_loop=1000 \
 				  --use_tpu=True \
-				  --tpu_name=chandrab-tpu-tutorial-2 \
+				  --tpu_name=chandrab-tpu-wsc-2 \
 				  --bert_large=True \
 				  --num_labels 2 \
 				  --task wsc
